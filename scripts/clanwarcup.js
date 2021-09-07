b2131e7fb89050652dce95820fa3732681c49ee2
Ian Angillis <ianangillis@gmail.com>
2021-09-07 15:45:18 +0200

    added tabs

---
+++ scripts/clanwarcup.js
@@ -0,0 +1,27 @@
+window.onload = function(){
+    tag = 'Upcoming'
+    tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tag).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openTab(evt, tag) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tag).style.display = "block";
+  evt.currentTarget.className += " active";
+}
